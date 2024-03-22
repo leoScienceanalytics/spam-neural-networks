ddd5d4660c407b71b28287f47699bf14976d22a3
Leonam Alves <leon14alv@gmail.com>
2024-03-22 16:10:54 -0300

    document updated

--- activate_spamfunctions.py
+++ activate_spamfunctions.py
@@ -1,12 +1,13 @@
-from spam_functions import processamento, tokenizer, rede_neural_treino, metrics_treino, rede_neural_test
-
+from spam_functions import processamento, tokenizer
+from spam_functions import rede_neural_treino, rede_neural_test, metrics_treino, metrics_test
+from spam_functions import graficos
 file = 'spam (1).csv'
 
 X_train, x_test, y_train, y_test = processamento(file)
 X_train, X_test, token = tokenizer(X_train, x_test)
 
 #Treinamento
-prev_treino, modelo = rede_neural_treino(X_train, y_train, token)
+prev_treino, modelo, loss_history_treino, accuracy_history_treino = rede_neural_treino(X_train, y_train, token)
 metricas = metrics_treino(y_test, prev_treino)
 print(metricas)
 
@@ -14,4 +15,6 @@ print(metricas)
 prev_test = rede_neural_test(X_test, modelo)
 print(prev_test)
 
+perda = graficos(loss_history_treino, accuracy_history_treino)
+print(perda)
 
--- spam_functions.py
+++ spam_functions.py
@@ -34,13 +34,14 @@ def rede_neural_treino(X_train, y_train, token):
     from keras.layers import Dense, Dropout, Flatten, Embedding
     
     
+    
     #Criando o modelo de redes neurais
     modelo = Sequential() #Cria modelo de rede neural sequencial
     #Configurando o modelo de redes neurais
     modelo.add(Embedding(input_dim=len(token.word_index), output_dim=50, input_length=500)) 
     #Cria uma camada que madeia cada token para um vetor de 50 dimensões
     modelo.add(Flatten()) #Achata a saída para um vetor de 1 dimensão
-    modelo.add(Dense(units=10, activation='relu')) 
+    modelo.add(Dense(units=10, activation='sigmoid')) 
     #Adiciona camada de 10 neurônios, com ReLu de função ativação
     modelo.add(Dropout(0.1)) #Adiciona camada dropout para perda de 10%, evitando overfitting
     modelo.add(Dense(units=1, activation='sigmoid')) 
@@ -48,17 +49,18 @@ def rede_neural_treino(X_train, y_train, token):
     modelo.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
     #Compila o modelo, definindo alguns parâmetros. Erro, Otimizador, e a métrica
     modelo.summary()
-    modelo.fit(X_train, y_train, epochs=20, batch_size=10, verbose=True, validation_data=(X_train, y_train))
+    modelo.fit(X_train, y_train, epochs=50, batch_size=10, verbose=True, validation_data=(X_train, y_train))
     #Treina o modelo com base nos conjuntos de treino
-    
+    loss_history_treino = modelo.history.history['loss']
+    accuracy_history_treino = modelo.history.history['accuracy']
     
     nova_previsao = modelo.predict(X_train)
     print(nova_previsao)
-    prev_treino = (nova_previsao > 0.5) 
+    prev_treino = (nova_previsao > 0.5)
     prev_treino = pd.DataFrame(prev_treino)
     print('Previsão de SPAM:')
     print(prev_treino)
-    return prev_treino[:1672], modelo
+    return prev_treino[:1672], modelo, loss_history_treino, accuracy_history_treino
     
 def metrics_treino(y_train, prev_treino):
     from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, classification_report
@@ -81,9 +83,9 @@ def metrics_treino(y_train, prev_treino):
     ax.yaxis.set_ticklabels(['True','False'])
     plt.show() #Matriz de Confusão
 
-    accuracy = accuracy_score(y_train, prev_treino)
-    accuracy = np.round(accuracy, 2)
-    print("Acurácia:", accuracy) #Acurácia
+    accuracy_treino = accuracy_score(y_train, prev_treino)
+    accuracy_treino = np.round(accuracy_treino, 2)
+    print("Acurácia:", accuracy_treino) #Acurácia
 
     precision = precision_score(y_train, prev_treino)
     precision = np.round(precision, 2)
@@ -101,11 +103,13 @@ def metrics_treino(y_train, prev_treino):
     target_names = ['ham', 'spam']
     report  = classification_report(y_train, prev_treino, target_names=target_names)
     print(report) #Resumo métricas
+    
+    return accuracy_treino
 
 def rede_neural_test(X_test, modelo):
     import pandas as pd
     
-   
+    
     nova_previsao = modelo.predict(X_test)
     print(nova_previsao)
     prev_test = (nova_previsao > 0.5) 
@@ -113,3 +117,65 @@ def rede_neural_test(X_test, modelo):
     print('Previsão de SPAM:')
     print(prev_test)
     return prev_test
+
+
+def metrics_test(y_test, prev_test):
+    from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, classification_report
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import numpy as np
+    
+    # Calculando as métricas
+    cm = confusion_matrix(y_test, prev_test) 
+    print('Matriz de Confusão:')
+    print(cm)#Matriz de Confusão
+    #Plotando a figura
+    fig = plt.figure(figsize=(5,5))
+    ax = sns.heatmap(cm/np.sum(cm), annot=True, fmt='.2%', cmap='Blues')
+    #Condigurações da figura
+    ax.set_title('Matriz de Confusão - SPAM\n\n')
+    ax.set_xlabel('\nValores Previstos')
+    ax.set_ylabel('Valores Reais')
+    ax.xaxis.set_ticklabels(['False','True'])
+    ax.yaxis.set_ticklabels(['True','False'])
+    plt.show() #Matriz de Confusão
+
+    accuracy_test = accuracy_score(y_test, prev_test)
+    accuracy_test = np.round(accuracy_test, 2)
+    print("Acurácia:", accuracy_test) #Acurácia
+
+    precision = precision_score(y_test, prev_test)
+    precision = np.round(precision, 2)
+    print("Precisão:", precision) #Precisão
+
+    recall = recall_score(y_test, prev_test)
+    recall = np.round(recall, 2)
+    print("Recall:", recall) #Sensibilidade
+
+    f1 = f1_score(y_test, prev_test)
+    f1 = np.round(f1, 2)
+    print("F1-Score:", f1) #F1-Score
+
+
+    target_names = ['ham', 'spam']
+    report  = classification_report(y_test, prev_test, target_names=target_names)
+    print(report) #Resumo métricas
+    
+    return accuracy_test
+
+
+def graficos(loss_history, accuracy_history_treino):
+    import matplotlib.pyplot as plt
+    
+    plt.plot(loss_history)
+    plt.xlabel('Época')
+    plt.ylabel('Loss')
+    plt.title('Evolução de Loss')
+    plt.show()
+    
+    
+    plt.plot(accuracy_history_treino)
+    plt.xlabel('Época')
+    plt.ylabel('Accuracy')
+    plt.title('Evoluação da Acurácia')
+    plt.show()
